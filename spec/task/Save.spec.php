4adc419f24f16833a4ac3701aad15e68b92bfc35
holyshared <holy.shared.design@gmail.com>
2014-12-29 13:27:58 +0900

    Update spec

--- spec/task/Save.spec.php
+++ spec/task/Save.spec.php
@@ -3,14 +3,14 @@
 use cloak\robo\coveralls\task\Save;
 
 describe('Save', function() {
-    describe('run', function() {
+    describe('#run', function() {
         beforeEach(function() {
             $this->configPath = __DIR__ . '/../fixtures/coveralls.toml';
-            $this->reportPath = __DIR__ . '/../fixtures/tmp/coveralls.json';
+            $this->reportPath = __DIR__ . '/../../tmp/build_coveralls.json';
 
-            $template = file_get_contents(__DIR__ . '/../fixtures/tmp/report.lcov');
+            $template = file_get_contents(__DIR__ . '/../fixtures/report.lcov');
             $reportContent = str_replace('{rootDirectory}', realpath(__DIR__ . '/../../'), $template);
-            file_put_contents(__DIR__ . '/../fixtures/tmp/compile_report.lcov', $reportContent);
+            file_put_contents(__DIR__ . '/../../tmp/build_report.lcov', $reportContent);
 
             $this->task = new Save($this->configPath);
         });
